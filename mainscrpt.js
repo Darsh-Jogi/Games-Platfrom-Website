b4ee70ab5fc11ad94b6f96887feeb58f3e647d42
Darsh-Jogi <158138473+Darsh-Jogi@users.noreply.github.com>
2024-01-31 00:33:27 +0530

    Update mainscrpt.js

--- mainscrpt.js
+++ mainscrpt.js
@@ -1,34 +1,34 @@
 console.log("javascript start from here")
 
 function redirectToGame1() {
-    window.open('/Game - 1/Memory Card Game/Memory Cards Game [Demo 1]/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 1/Memory Card Game/Memory Cards Game [Demo 1]/index.html', '_blank'); // Adjust the path accordingly
   }
 function redirectToGame2() {
-    window.open('/Game - 2/Typing Speed Test Game in JavaScript/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 2/Typing Speed Test Game in JavaScript/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame3() {
-    window.open('/Game - 3/build-hangman-game-html-javascript/Hangman Game/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 3/build-hangman-game-html-javascript/Hangman Game/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame4() {
-    window.open('/Game - 4/Quiz Application with Timer Game/Quiz Application with Timer/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 4/Quiz Application with Timer Game/Quiz Application with Timer/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame5() {
-    window.open('/Game - 5/Word Scramble Game/Word Scramble/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 5/Word Scramble Game/Word Scramble/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame6() {
-    window.open('/Game - 6/Tic Tac Toe Game/Tic Tac Toe Javascript game/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 6/Tic Tac Toe Game/Tic Tac Toe Javascript game/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame7() {
-    window.open('/Game - 7/Number Guessing Game/Game using HTML CSS & JavaScript/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 7/Number Guessing Game/Game using HTML CSS & JavaScript/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame8() {
-    window.open('/Game - 8/Word Guessing Game/Word Guessing/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 8/Word Guessing Game/Word Guessing/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame9() {
-    window.open('/Game - 9/Rock Paper Scissors/Rock Paper Scissors/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 9/Rock Paper Scissors/Rock Paper Scissors/index.html', '_blank'); // Adjust the path accordingly
 }
 function redirectToGame10() {
-    window.open('/Game - 10/Snake Game in JavaScript/Snake Game/index.html', '_blank'); // Adjust the path accordingly
+    window.open('Game - 10/Snake Game in JavaScript/Snake Game/index.html', '_blank'); // Adjust the path accordingly
 }
 
 // feedback javascrpt code
